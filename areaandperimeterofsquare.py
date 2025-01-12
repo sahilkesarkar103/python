808d22c30dac6bb8d22df75cd1e5b8bc6b118b2a
sahilkesarkar103 <156207517+sahilkesarkar103@users.noreply.github.com>
2025-01-12 22:15:03 +0530

    Create areaandperimeterofsquare.py

---
+++ areaandperimeterofsquare.py
@@ -0,0 +1,12 @@
+
+
+
+side = float(input("Enter the length of the side of the square: "))
+
+
+area = side ** 2
+perimeter = 4 * side
+
+
+print(f"The area of the square is: {area}")
+print(f"The perimeter of the square is: {perimeter}")
