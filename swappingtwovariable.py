9c4500c56abe3bbaafc31218a5f9e855221c8b2d
sahilkesarkar103 <156207517+sahilkesarkar103@users.noreply.github.com>
2025-01-12 22:18:15 +0530

    Create swappingtwovariable.py

---
+++ swappingtwovariable.py
@@ -0,0 +1,12 @@
+
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+
+
+temp = a
+a = b
+b = temp
+
+print("After swapping:")
+print("Value of a:", a)
+print("Value of b:", b)
